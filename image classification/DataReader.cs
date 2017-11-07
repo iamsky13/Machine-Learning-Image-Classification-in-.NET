a1e06055d706824b5bf04c1ec345084e859edfda
Aakash <uniqueaakash13@hotmail.com>
2017-11-07 22:21:12 +0545

    added data reader

--- image classification/DataReader.cs
+++ image classification/DataReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,26 @@ namespace image_classification
 {
     public class DataReader
     {
+        private static Observation ObservationFactory(string data)
+        {
+            var commaSeparated = data.Split(',');
+            var label = commaSeparated[0];
+            var pixels =
+            commaSeparated
+            .Skip(1)
+            .Select(x => Convert.ToInt32(x))
+            .ToArray();
+            return new Observation(label, pixels);
+        }
+        public static Observation[] ReadObservations(string dataPath)
+        {
+            var data =
+            File.ReadAllLines(dataPath)
+            .Skip(1)
+            .Select(ObservationFactory)
+            .ToArray();
+            return data;
+        }
     }
+
 }
